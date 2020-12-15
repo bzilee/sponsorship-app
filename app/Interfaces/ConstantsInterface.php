827b8b805de41900abc76872bf5158a626362c22
Tankeu Bzile <b.ziletankeu@gmail.com>
2020-12-15 18:54:37 +0100

    Ajout du fichier de constantes dynamiques

---
+++ app/Interfaces/ConstantsInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2019. Tankeu B.zile
+ * @author    Tankeu B.zile <b.ziletankeu@gmail.com>
+ * @author    Chinzoumka Tchindebe <christiantchindebe@outlook.fr>
+ * @link      https://github.com/TankeuBzile
+ */
+
+namespace App\Interfaces;
+
+interface ConstantsInterface
+{
+    public static const SEPARATOR = DIRECTORY_SEPARATOR ;
+}
